5e87b087e593a9ac8f7e5747237bae5e059b3491
wangmengling <294025529@qq.com>
2018-06-11 16:16:55 +0800

    Revert "webpack 修改"
    This reverts commit b05d8e5d514a9f498f848d0cbf51a903f4f6df52.

--- src/Routes.js
+++ src/Routes.js
@@ -10,6 +10,7 @@ import history from "./History";
 import DefaultLayout from "./components/layout/DefaultLayout";
 import App from './App';
 <<<<<<< HEAD
+<<<<<<< HEAD
 import { listCase,CaseDetail } from "./modules/case";
 
 // class App extends Component {
@@ -24,15 +25,22 @@ import { listCase,CaseDetail } from "./modules/case";
 =======
 import { List } from "./modules/case";
 >>>>>>> parent of b05d8e5... webpack 修改
+=======
+import { List } from "./modules/case";
+>>>>>>> parent of b05d8e5... webpack 修改
 
 const Routes = () => (
     // <Router>
     <Router history={history}> 
       <div>
 <<<<<<< HEAD
+<<<<<<< HEAD
         <DefaultLayout exact path="/" component={listCase}/> 
         <DefaultLayout exact path="/case/detail" component = {CaseDetail} />
         {/* <Route exact path="/" component={App}/> */}
+=======
+        <DefaultLayout exact path="/" component={List}/>
+>>>>>>> parent of b05d8e5... webpack 修改
 =======
         <DefaultLayout exact path="/" component={List}/>
 >>>>>>> parent of b05d8e5... webpack 修改
--- src/index.js
+++ src/index.js
@@ -5,9 +5,14 @@ import './index.less';
 // import App from './App';
 import Routes from "./Routes";
 <<<<<<< HEAD
+<<<<<<< HEAD
 // import registerServiceWorker from './registerServiceWorker';
 // const electron = require('electron')
 // const fs = require('fs')
+=======
+import registerServiceWorker from './registerServiceWorker';
+
+>>>>>>> parent of b05d8e5... webpack 修改
 =======
 import registerServiceWorker from './registerServiceWorker';
 
